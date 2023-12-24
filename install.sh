b371ab2001b52f89f47a5caaa46c91b7032f0340
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2023-12-24 14:13:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ if [ $EUID != 0 ]; then
     sudo "$0" "$@"
     exit $?
 fi
-bash <(curl https://ddurl.imjcj.eu.org/jJsgY)
+bash <(curl https://gh.imjcj.eu.org/https://github.com/Jun-Software/Jun-Online-Judge/raw/master/nix.sh)
 nix-env -i git
 git clone https://gh.imjcj.eu.org/https://github.com/Jun-Software/Jun-Online-Judge
 pip3 install -r requirements.txt
