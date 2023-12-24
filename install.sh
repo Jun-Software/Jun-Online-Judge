1b9eae9d876869d22b8e70ade28f5e78fd0cd204
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2023-12-24 14:15:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
-if [ $EUID != 0 ]; then
-    sudo "$0" "$@"
-    exit $?
-fi
 bash <(curl https://gh.imjcj.eu.org/https://github.com/Jun-Software/Jun-Online-Judge/raw/master/nix.sh)
 nix-env -i git
 git clone https://gh.imjcj.eu.org/https://github.com/Jun-Software/Jun-Online-Judge
