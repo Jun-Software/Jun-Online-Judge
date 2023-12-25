65cbc2f881489f351cd28e2b0b1cf9d284ba4df3
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2023-12-25 22:25:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,29 @@ admin_password = 'admin'
 judge_language = 'Python 3'
 judge_command = 'python {}'
 # judge_command = 'g++ {} -o a.out -w -std=c++11 && ./a.out'
+
+problem_template = '''
+## [题目名称]
+<hr/>
+
+### 题目详情
+[题目详情]
+<hr/>
+
+
+
+##### 输入样例#1
+[输入样例#1]
+<hr/>
+
+
+##### 输出样例#1
+[输出样例#1]
+<hr/>
+
+
+
+#### 数据范围
+<p>数据范围</p>
+<hr/>
+'''
