1ad5a6b63a74e15132db3ee10002efb0b61c5640
lemonorangeapple <jcj1947725596@hotmail.com>
2023-12-26 20:20:55 +0800

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
-bash <(curl https://gh.imjcj.eu.org/https://github.com/Jun-Software/Jun-Online-Judge/raw/master/nix.sh)
+apt-get install git
+yum install git
+dnf install git
+emerge --ask --verbose dev-vcs/git
+pacman -S git
+zypper install git
+urpmi git
 nix-env -i git
+pkg install git
+pkgutil -i git
+pkg install developer/versioning/git
+pkg_add git
+apk add git
+tazpkg get-install git
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 git clone https://gh.imjcj.eu.org/https://github.com/Jun-Software/Jun-Online-Judge
 pip3 install -r requirements.txt
