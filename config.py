dc7f466ee8e2f3d72b0e9251c1e3bba1d57b2b5d
lemonorangeapple <jcj1947725596@hotmail.com>
2024-02-06 21:28:42 +0800

    update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 admin_password = 'admin'
 judge_language = 'C++ 11'
 # judge_command = 'python {}'
-judge_command = 'g++ {} -o a.out -w -std=c++11; ./a.out'
+judge_command = 'g++ {} -o a.out -w -std=c++11 | ./a.out'
 
 problem_template = '''
 ## [题目名称]
